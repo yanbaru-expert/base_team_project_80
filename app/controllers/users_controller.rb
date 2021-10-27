489f0fb3335871de39c98690bd8ab63765fadce3
numa-create <k.numata0815@gmail.com>
2021-10-27 16:36:39 +0900

    6.編集機能 edit, update を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def index
       @users = User.all
     end
-    # 以下のアクションを追加
+    
     def show
       @user = User.find(params[:id])
     end
@@ -13,6 +13,15 @@ class UsersController < ApplicationController
   
     def create
       User.create(user_params)
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
     end
   
     private
