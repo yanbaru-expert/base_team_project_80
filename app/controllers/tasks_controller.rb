624cd7c6e5bb40b026a3451a0132900daac3f2e5
atsu-mi <atumi0205@gmail.com>
2021-10-31 17:07:44 +0900

    4.一覧表示機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,8 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
   end
@@ -6,7 +10,7 @@ class TasksController < ApplicationController
   def create
     Task.create(task_params)
   end
-  
+
   private
   def task_params
     params.require(:task).permit(:title, :content)
